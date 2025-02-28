36d1d86cd7e21ef90ebef34c960738a8943c6d87
TuanPh1608 <ptuan160804@gmail.com>
2025-02-28 15:38:34 +0700

    Install MUI and CSSBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import CSSBaseline from '@mui/material/CssBaseline'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CSSBaseline />
     <App />
   </StrictMode>,
 )
