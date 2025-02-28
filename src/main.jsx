518f2c097f82ae7a67239cd4d01915f815a41ab9
TuanPh1608 <ptuan160804@gmail.com>
2025-02-28 17:55:51 +0700

    Config dark-light mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CSSBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CSSBaseline />
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme} defaultMode='light'>
+      <CSSBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
---
+++ src/theme.js
@@ -0,0 +1,49 @@
+
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+// Create a theme instance.
+const theme = createTheme({
+    cssVariables: {
+        colorSchemeSelector: '.mode-%s',
+    },
+    colorSchemes: {
+        dark: {
+            palette: {
+                primary: {
+                    main: '#90caf9',
+                },
+                secondary: {
+                    main: '#f48fb1',
+                },
+                error: red,
+                background: {
+                    default: '#303030',
+                    paper: '#424242',
+                },
+                text: {
+                    primary: 'rgba(255, 255, 255, 0.87)',
+                    secondary: 'rgba(255, 255, 255, 0.7)',
+                    disabled: 'rgba(255, 255, 255, 0.5)',
+                }
+            },
+        },
+        light: {
+            palette: {
+                primary: {
+                    main: '#1976d2',
+                },
+                secondary: {
+                    main: '#dc004e',
+                },
+                error: red,
+                background: {
+                    default: '#fafafa',
+                    paper: '#fff',
+                },
+            },
+        }
+    },
+})
+
+export default theme
