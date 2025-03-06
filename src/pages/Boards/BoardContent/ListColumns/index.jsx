0099b5b02d1a8d0fdeca59f535c3fa9cd3d8b81a
TuanPh1608 <ptuan160804@gmail.com>
2025-03-07 00:12:39 +0700

    Basic dnd for trello card

--- src/pages/Boards/BoardContent/ListColumns/index.jsx
+++ src/pages/Boards/BoardContent/ListColumns/index.jsx
@@ -3,47 +3,55 @@ import Column from './Column'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
 import Button from '@mui/material/Button'
 
-function ListColumns({ columns }) {
-    return (
-        <Box sx={{
-            bgcolor: 'inherit',
-            width: '100%',
-            height: '100%',
-            overflowX: 'auto',
-            overflowY: 'hidden',
-            display: 'flex',
-            '&::webkit-scrollbar-track': { m: 2 }
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
-        }}>
-            {/* Column */}
-            {columns?.map(column => {
-                return <Column key={column._id} column={column} />
-            })}
-            {/* Add more columns */}
-            <Box
-                sx={{
-                    minWidth: '250px',
-                    maxWidth: '250px',
-                    mx: 2,
-                    borderRadius: '8px',
-                    height: 'fit-content',
-                    bgcolor: '#ffffff3d'
-                }}
-            >
-                <Button
-                    startIcon={<NoteAddIcon />}
+
+function ListColumns({ columns }) {
+    /* https://github.com/clauderic/dnd-kit/issues/183#issuecomment-812569512 */
+    return (
+        <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
+            <Box sx={{
+                bgcolor: 'inherit',
+                width: '100%',
+                height: '100%',
+                overflowX: 'auto',
+                overflowY: 'hidden',
+                display: 'flex',
+                '&::webkit-scrollbar-track': { m: 2 }
+
+            }}>
+                {/* Column */}
+                {
+                    columns?.map(column => {
+                        return <Column key={column._id} column={column} />
+                    })
+                }
+                {/* Add more columns */}
+                <Box
                     sx={{
-                        color: 'white',
-                        width: '100%',
-                        justifyContent: 'flex-start',
-                        pl: 3,
-                        py: 1
+                        minWidth: '250px',
+                        maxWidth: '250px',
+                        mx: 2,
+                        borderRadius: '8px',
+                        height: 'fit-content',
+                        bgcolor: '#ffffff3d'
                     }}
                 >
-                    Add new column
-                </Button>
-            </Box>
-        </Box >
+                    <Button
+                        startIcon={<NoteAddIcon />}
+                        sx={{
+                            color: 'white',
+                            width: '100%',
+                            justifyContent: 'flex-start',
+                            pl: 3,
+                            py: 1
+                        }}
+                    >
+                        Add new column
+                    </Button>
+                </Box>
+            </Box >
+        </SortableContext >
     )
 }
 
